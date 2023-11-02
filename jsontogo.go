9ca50d2929eacb5032bf65c5d927c0167c127ab6
cs50-romain <cs50.git.romain@gmail.com>
2023-11-01 20:29:24 -0400

    Syntax changes & created tokenizer

--- jsontogo.go
+++ jsontogo.go
@@ -27,23 +27,18 @@ var (
 // String starts with " and ends with " but is always after a :
 // Punctuator is either { } : [ ] ,
 
-type String struct {
-	type string //String
+type Token struct {
+	lexeme string //String
 	value string
 }
 
-type Punctuator struct {
-	type string // Punctuator
-	value rune
-}
+var tokens []Token
 
 // Tokenizer. Create an array of tokens for now
 func lexer() {
-	reader()
-}
+	chars := []rune{}
+	var charBuffer string
 
-// Read from file
-func reader() {
 	file, err := os.Open("sample.json")
 	if err != nil {
 		fmt.Println("Error:",err)
@@ -52,48 +47,82 @@ func reader() {
 
 	scanner := bufio.NewScanner(file)
 
+	// Append characters to array for simpler pass through
 	for scanner.Scan() {
 		line := scanner.Text()
 		fmt.Println("Line: ", line)
-		readLine(line)
+		for _, char := range line {
+			chars = append(chars, char)
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		fmt.Println(err)
 	}
-}
 
-func readLine(line string){
-	for _, char := range line {
-		fmt.Print("Char:", string(char))
-		readChar(char)
+	// Tokenizing 
+	for i := 0; i < len(chars); i++ {
+		char := chars[i]
+		useBuffer := readChar(char)
+
+		if !useBuffer {
+			if len(charBuffer) > 0 {
+				token := Token{
+					lexeme: "string",
+					value: charBuffer,
+				}
+				tokens = append(tokens, token)
+				charBuffer = ""
+			}
+
+			token := Token{
+				lexeme: "punctuator",
+				value: string(char),
+			}
+			tokens = append(tokens, token)
+		} else {
+			charBuffer += string(char)			
+		}
 	}
+
+	fmt.Println(tokens)
 }
 
 //read character
-func readChar(ch rune) {
+func readChar(ch rune) bool {
 	if ch == W_SPACE {
 		fmt.Println(" whitespace")
+		return true 
 	} else if ch == LEFT_CB {
 		fmt.Println(" left curly bracket")
+		return false
 	} else if ch == RIGHT_CB {
 		fmt.Println(" right curly bracket")
+		return false
 	} else if ch == LEFT_PA {
 		fmt.Println(" left parentheses")
+		return false
 	} else if ch == RIGHT_PA {
 		fmt.Println(" right parentheses")
+		return false
 	} else if ch == DD {
 		fmt.Println(" double dot")
+		return false
 	} else if ch == COMA {
 		fmt.Println(" comma")
+		return false
 	} else if ch == LEFT_BR {
 		fmt.Println(" left bracket")
+		return false
 	} else if ch == RIGHT_BR {
 		fmt.Println(" right bracket")
+		return false
 	} else if ch == QUOTE {
 		fmt.Println(" quote")
+		return false
 	} else {
 		fmt.Println(" character")
+		return true 
 	}
 }
 
