0e6baa5d63ccb2574b0b1be53117a0d932ae5742
cs50-romain <cs50.git.romain@gmail.com>
2023-11-23 19:43:37 -0500

    Added benchmark function

--- jsontogo_test.go
+++ jsontogo_test.go
@@ -1,3 +1,18 @@
 package main
 
 import "testing"
+import "strings"
+
+func BenchmarkTraversal(b *testing.B) {
+	node1 := &Node{"string", "value2", 2 /*level*/, nil}
+	node2 := &Node{"string", "value3", 2 /*level*/, nil}
+	node3 := &Node{"string", "value4", 2 /*level*/, nil}
+	head := &Node{"struct", "value1", 1 /*level*/, nil}
+	head.children = append(head.children, node1, node2, node3)
+	var bs strings.Builder
+
+	tree := InitAST()
+	tree.head = head
+
+	_ = tree.traversal(head, false, bs)
+}
