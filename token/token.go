0137eb815da597ab574220df45e391c2bd17f009
cs50-romain <cs50.git.romain@gmail.com>
2024-01-01 15:48:04 -0500

    Added token type, and possible tokens

---
+++ token/token.go
@@ -0,0 +1,23 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type 	TokenType
+	Literal	string
+}
+
+const (
+	LBRACE = "{"
+	RBRACE = "}"
+	LBRACK = "["
+	RBRACK = "]"
+
+	COMMA = ","
+	DD = ":"
+	PARENTH = "\""
+	
+	KEY = "KEY" // Any key in a key:value pair
+	INT = "INT" // 1, 2, 34...
+	STRING = "STRING" // Any value in a key:value pair
+)
